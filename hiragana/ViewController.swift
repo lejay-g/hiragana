a79eebb5a19f043c8d520ed53527ca30b47ac8d3
shokoni <nishida.shoko.km@gmail.com>
2021-01-02 22:39:44 +0900

    Initial Commit

---
+++ hiragana/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hiragana
+//
+//  Created by 西田祥子 on 2021/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
