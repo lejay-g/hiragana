a4176325e3395bc17c7b7af274ee7769c7cbf4a4
shokoni <nishida.shoko.km@gmail.com>
2021-01-02 22:45:48 +0900

    ストーリーボード削除

---
+++ hiragana/mainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  mainViewController.swift
+//  hiragana
+//
+//  Created by 西田祥子 on 2021/01/02.
+//
+
+import UIKit
+
+class mainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
